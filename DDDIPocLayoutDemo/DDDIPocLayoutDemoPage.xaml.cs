a5b044442d05d37d53d8479d6319023f684b3a32
vishal tahil <vishal.tahil@hotmail.com>
2017-12-24 00:58:56 -0600

    First commit

---
+++ DDDIPocLayoutDemo/DDDIPocLayoutDemoPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Xamarin.Forms;
+
+namespace DDDIPocLayoutDemo
+{
+    public partial class DDDIPocLayoutDemoPage : ContentPage
+    {
+        void onPageSizeChanged(object sender, System.EventArgs e)
+        {
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                if (Width < Height)
+                { //portrait mode
+                    parentLoginGrid.Margin = new Thickness(0, 70, 0, 0);
+                    parentLoginGrid.ColumnDefinitions[1].Width = new GridLength(300, GridUnitType.Absolute);
+
+                }
+                else
+                { //landscape mode
+                    parentLoginGrid.Margin = new Thickness(0, 0, 0, 0);
+                    parentLoginGrid.ColumnDefinitions[1].Width = new GridLength(400, GridUnitType.Absolute);
+                }
+            }
+            else //tablet mode
+            {
+                parentLoginGrid.Margin = new Thickness(0, 70, 0, 0);
+                parentLoginGrid.ColumnDefinitions[1].Width = new GridLength(400, GridUnitType.Absolute);
+            }
+        }
+
+
+        public DDDIPocLayoutDemoPage()
+        {
+            InitializeComponent();
+            this.txtPassword.Placeholder = "Password";
+            this.txtUserName.Placeholder = "User Name";
+
+        }
+
+
+
+    }
+}
